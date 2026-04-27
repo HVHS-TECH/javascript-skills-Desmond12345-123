f9823bb09d71c47a293f658686c79faf983c7db7
Desmond <24415dl@hvhs.school.nz>
2026-04-27 21:51:15 +0000

    Apr 27, 2026, 9:51 PM

--- t03_maths.js
+++ t03_maths.js
@@ -7,17 +7,17 @@ console.log("Hello, World!")
 
 // This is a single line comment
 
-var name = "Desmond"
+var name = "Desmond";
 console.log(name)
 
-var age = 15
+var age = 15;
 console.log(age)
 
 var isStudent = true
 console.log("My name is "+ name + " and I am " + age + " years old. Am I a student? " + isStudent)
 console.log("You were born in " + (2026 - age))
-console.log("In 20 years, you will be " + (age + 20)
-var money = 100;
-console.log("You have $" + money)
-console.log("Half your money you have $" + money/2)
-console.log("If you get $3 than you will have $" + (money + 3))
+console.log("In 20 years, you will be " + (age + 20))
+var num1 = 100;
+console.log("You have $" + num1)
+console.log("Half your money you have $" + num1/2)
+console.log("If you get $3 than you will have $" + (num1 + 3))
