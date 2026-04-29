a1c1a7d81ef747d993d62841117959d47030be83
Desmond <24415dl@hvhs.school.nz>
2026-04-29 00:02:20 +0000

    Apr 29, 2026, 12:02 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,14 +1,11 @@
 console.log("Task 4 - Debugging");
 
-// calculate the area
-var area = length * width;
-
-// calculate the perimeter
-var perimeter = (2 * length + 2 * width);
-    
-// Set up the size of the page.
+// Variables
 var length = 8;
 var width = 10;
+var area = (length * width);
+var perimeter = (2 * length + 2 * width);
+
 
 // Display the results
 console.log("A area of a piece of paper with a length of " + length + ", and a width of "+ width + " is "+ area)
