6f839d2319530d29bb621a9e3e4dfd2529e30c1f
Desmond <24415dl@hvhs.school.nz>
2026-04-29 00:03:40 +0000

    Apr 29, 2026, 12:03 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,23 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Hello, World!")
+
+// This is a single line comment
+
+var name = "Desmond";
+console.log(name)
+
+var age = 15;
+console.log(age)
+
+var isStudent = true
+console.log("My name is "+ name + " and I am " + age + " years old. Am I a student? " + isStudent)
+console.log("You were born in " + (2026 - age))
+console.log("In 20 years, you will be " + (age + 20))
+var num1 = 100;
+console.log("You have $" + num1)
+console.log("Half your money you have $" + num1/2)
+console.log("If you get $3 than you will have $" + (num1 + 3))
