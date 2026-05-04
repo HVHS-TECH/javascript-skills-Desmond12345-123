978cfe1b85ac0c407a4676926f367e42f02297b7
Desmond <24415dl@hvhs.school.nz>
2026-05-04 21:38:42 +0000

    May 4, 2026, 9:38 PM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,1 +1,76 @@
-/* Paste your code from the last task */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+const output = document.getElementById("spaceForJavaScriptOutput")
+output.innerHTML = "This is the output of JavaScript code. You can replace this text with your own output by using JavaScript to change the innerHTML of this element."
+console.log("Running t01_introduction.js")
+console.log("Hello, World!")
+
+// This is a single line comment
+
+var name = "Desmond";
+console.log(name)
+
+var age = 15;
+console.log(age)
+
+var isStudent = true
+console.log("My name is "+ name + " and I am " + age + " years old. Am I a student? " + isStudent)
+console.log("You were born in " + (2026 - age))
+console.log("In 20 years, you will be " + (age + 20))
+var num1 = 100;
+console.log("You have $" + num1)
+console.log("Half your money you have $" + num1/2)
+console.log("If you get $3 than you will have $" + (num1 + 3))
+
+function welcome(name) {
+    output.innerHTML = "<p>Welcome to the shop</p>" + name
+}
+
+welcome(name);
+
+
+
+
+
+function start() {
+    output.innerHTML += "<p>Button was clicked!</p>"
+}
+
+
+function getFormInput() {
+    const nameField = document.getElementById("nameField");
+    userName = nameField.value;
+    output.innerHTML += "<p>Hello, " + userName + "!</p>"
+}       
+
+
+function getAge () {
+    const ageField = document.getElementById("ageField");
+    userAge = ageField.value;
+    output.innerHTML += "<p>You are " + userAge + " years old.</p>"
+}
+
+function getUserPocketMoney () {
+    const pocketMoneyField = document.getElementById("pocketMoneyField");
+    userPocketMoney = pocketMoneyField.value;
+    output.innerHTML += "<p>You have $" + userPocketMoney + " in your pocket.</p>"
+    if (userPocketMoney >= 4) {
+        output.innerHTML += "<p>You CAN afford a chocolate bar!</p>"
+    } else {
+        output.innerHTML += "<p>You CANNOT afford a chocolate bar!</p>"
+    }
+    event.preventDefault();
+}
+
+function caclulateChange(_money, _price){
+    var change = (_money - _price);
+    console.log("Your change is $" + change);
+    output.innerHTML += "<p>Your change is $" + change + "</p>";
+    return change;
+}
+
+getFormInput()
+getUserPocketMoney()
+getAge()
