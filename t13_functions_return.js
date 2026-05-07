8590082f405cceabf2605718eb07fb285361263d
Desmond <24415dl@hvhs.school.nz>
2026-05-07 01:20:25 +0000

    May 7, 2026, 1:20 AM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -70,6 +70,7 @@ function caclulateChange(userPocketMoney, chocolateBarPrice){
 
     let chocolateBarPrice = 4;
     let change = (userPocketMoney - chocolateBarPrice);
+    <p>hello</p>
     output.innerHTML += "<p>hello</p>";
     output.innerHTML += "<p>If you buy the chocolate bar, your change is $" + change + "</p>";
 }
@@ -77,3 +78,4 @@ function caclulateChange(userPocketMoney, chocolateBarPrice){
 getFormInput();
 getUserPocketMoney();
 getAge();
+caclulateChange(userPocketMoney, chocolateBarPrice);
